a2d3fed9271c734efb65021189d5cbcc349c1105
aaron-might <aaronmighty7@gmail.com>
2021-05-18 17:10:32 -0400

    update script and index.html and script

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,9 @@
 // Assignment Code
-var passwordLength =[8,9,10,] 
-var uppercase = [A,B,C,D,E,F,G,H,I,J,K,L,M,N,]
-var lowercase = [ a,b,c,d,e,f,g,h,i,]
-var numeric = [0,1,2,3,4,5,6,7,8,9]
-var specialCharacters = [ ~,!,@var,#,$,&,%,^,*,+=,(),>,<,]
-
+//var passwordLength = [8,9,10,11,12] 
+var upperCase  = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var lowerCase = "lowerupperCase";
+var numeric = [1,2,3,4,5,6,7,8,9];
+var specialCharacters = ["~","!","@","#","&","%","^","*","=",">"];
 
 
 var generateBtn = document.querySelector("#generate");
@@ -47,7 +46,7 @@ var specialCharacters = confirm("do you want specialCharacters?")
 console.log(specialCharacters)
 
 
-return " ";
+return "Password";
 }
 
 
